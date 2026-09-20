b646a9d6464f783db4cb4572eab46c5c9eab0d63
Tith Praservatanak <hig694039@gmail.com>
2026-09-20 22:00:58 +0700

    feat: added new route

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoute.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 dotenv.config();
-
-const app = express();
-app.use(errorHandler);
-
-app.use("/api/auth", authRoutes);
-
 ConnectDb();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const app = express();
 
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/todos", todoRoutes);
+
+app.use(errorHandler);
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
